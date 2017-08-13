bb0ef0152d4cb1308a71af4dc96906d68e426945
Ran Endelman <endelmanran@gmail.com>
2017-08-13 21:41:42 +0300

    Added new home screen, stop timers at the end of the game and more

--- Frogs/CollectionViewController.swift
+++ Frogs/CollectionViewController.swift
@@ -14,6 +14,8 @@ private let numOfSections = 1
 private var score = 0
 private var hits = 3
 private var counter = 60
+private var timer: Timer?
+private var timerForChangeBackgroundImages: Timer?
 
 class CollectionViewController: UICollectionViewController {
 
@@ -21,24 +23,11 @@ class CollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
 
-        var timerForChangeBackgroundImages = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(changeBackgroundImage), userInfo: nil, repeats: true)
+        timerForChangeBackgroundImages = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(changeBackgroundImage), userInfo: nil, repeats: true)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-    }
-
-    /* This method called by timer count down the time for the game */
-    func updateCounter() {
-        if counter > 0 {
-            print("00:\(counter)")
-            if counter == 60 {
-                gameTitle.title = "01:00"
-            } else {
-                gameTitle.title = "00:\(counter)"
-            }
-            counter -= 1
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -100,17 +89,59 @@ class CollectionViewController: UICollectionViewController {
 //            scoreBoard.text = "The score is :" + String(score)
         } else {
             hits -= 1
+            if hits < 0 {
+                showEndOfGameAlert(title: "Game Over! You are out of Hits!")
+            }
         }
         print("The score is :" + String(score))
         print("The hits is :" + String(hits))
     }
 
+    /* This method return back to the root viewController */
+    func finishGame() {
+        _ = navigationController?.popToRootViewController(animated: true)
+    }
+
+    /* This method called by timer count down the time for the game */
+    func updateCounter() {
+        if hits < 0 {
+            stopTimers()
+        }
+        if counter >= 0 {
+            print("00:\(counter)")
+            if counter == 60 {
+                gameTitle.title = "01:00"
+            } else if counter < 10 {
+                gameTitle.title = "00:0\(counter)"
+            } else {
+                gameTitle.title = "00:\(counter)"
+            }
+            counter -= 1
+        } else {
+            print("OUT OF TIME!")
+            stopTimers()
+            showEndOfGameAlert(title: "You are out of time!")
+        }
+    }
+
+    /* This method generate and show no more hits alert */
+    func showEndOfGameAlert(title: String) {
+        let alert = UIAlertController(title: title, message: "Your score is: \(score)", preferredStyle: UIAlertControllerStyle.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: {(alert: UIAlertAction!) in self.finishGame()})
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+    }
+
+    /* This method determine if the cell clicked contain frog image */
     func isFrogPic(collectionView: UICollectionView, indexPath: IndexPath) -> Bool {
         return collectionView.cellForItem(at: indexPath)?.alpha == 100
     }
 
     /* This method change all the Cell's background image, called by timerForChangeBackgroundImages */
     func changeBackgroundImage() {
+        if counter <= 0 || hits < 0 {
+            stopTimers()
+        }
         for cell in collectionView?.visibleCells as! [UICollectionViewCell] {
             var random_number = Int(arc4random_uniform(12) + 1)
             let image = UIImage(named: String(random_number) + ".jpg")
@@ -127,9 +158,15 @@ class CollectionViewController: UICollectionViewController {
         print("Changed background")
     }
 
+    /* This method stop the two timers */
+    func stopTimers() {
+        timer?.invalidate()
+        timer = nil
+        timerForChangeBackgroundImages?.invalidate()
+        timerForChangeBackgroundImages = nil
+    }
+
     // MARK: UICollectionViewDelegate
-
-
     // Uncomment this method to specify if the specified item should be highlighted during tracking
 //    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
 //        return true
