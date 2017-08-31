d1d98de379496349e2d8e34ae36ffcced3a96964
Ran Endelman <endelmanran@gmail.com>
2017-08-31 17:20:36 +0300

    FireBase installed to the app

---
+++ Pods/Target Support Files/Pods-Frogs/Pods-Frogs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Frogs : NSObject
+@end
+@implementation PodsDummy_Pods_Frogs
+@end
---
+++ Pods/Target Support Files/Pods-FrogsTests/Pods-FrogsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FrogsTests : NSObject
+@end
+@implementation PodsDummy_Pods_FrogsTests
+@end
