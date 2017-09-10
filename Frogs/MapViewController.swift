133b57b2dfadb44bb0d37114d9589fc45b5cdd69
Ran Endelman <endelmanran@gmail.com>
2017-09-10 12:58:20 +0300

    Every record contain location, click on record in the table links to map with the location

--- Frogs/MapViewController.swift
+++ Frogs/MapViewController.swift
@@ -6,14 +6,36 @@
 import Foundation
 import MapKit
 import CoreLocation
+import FirebaseDatabase
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var map: MKMapView!
+    let manager = CLLocationManager()
+    private var ref: DatabaseReference!
+    var clickedRow: Int = -1
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.ref = Database.database().reference()
+        self.setLatAndLong()
     }
 
-
-
+    func setLatAndLong() {
+        self.ref.child("highScores").observeSingleEvent(of: .value, with: { (snapshot) in
+            var arr = snapshot.value as? [[String: Any]]
+            var clickedDic: [String: Any] = arr![self.clickedRow]
+            var lat: Double = clickedDic["lat"] as! Double
+            var long: Double = clickedDic["long"] as! Double
+            let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+            let recordLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long)
+            let region:MKCoordinateRegion = MKCoordinateRegionMake(recordLocation, span)
+            self.map.setRegion(region, animated: true)
+            self.map.showsUserLocation = true
+            self.map.showsCompass = true
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
 
 
 }
